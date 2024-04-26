4e21040b7e9a5ce3a4624d318faa79f570e121d8
Tony Liam Merrigan <Tony.Liam.Merrigan@gmail.com>
2024-04-26 13:43:16 +0000

    Adding convenience script for running in WSL2

---
+++ scripts/wsl2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p $HOME/.gitkraken
+podman run -d -it --privileged=true --rm -v /mnt/wslg/.X11-unix:/tmp/.X11-unix\
+               -v /mnt/wslg:/mnt/wslg\
+               -v $HOME/github:/root/github\
+               -v $HOME/.gitconfig:/root/.gitconfig\
+               -v $HOME/.gitkraken:/root/.gitkraken\
+               -v $HOME/.config:/root/.config\
+               -e DISPLAY=$DISPLAY\
+               -e WAYLAND_DISPLAY=wayland-0\
+               -e XDG_RUNTIME_DIR=/mnt/wslg/runtime-dir\
+               -e PULSE_SERVER=unix:/mnt/wslg/PulseServer\
+               ghcr.io/whynotea/gitkraken:latest /usr/share/gitkraken/gitkraken --no-sandbox --disable-gpu
