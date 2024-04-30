f6742c358dc59157f16b2175c1945465540f24e1
Tony Liam Merrigan <Tony.Liam.Merrigan@gmail.com>
2024-04-30 14:36:11 +0100

    podman-compose support

--- scripts/wsl2.sh
+++ scripts/wsl2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+podman network create --internal gitkraken_internal
 mkdir -p $HOME/.gitkraken
 podman run -d -it --privileged=true --rm -v /mnt/wslg/.X11-unix:/tmp/.X11-unix\
                -v /mnt/wslg:/mnt/wslg\
@@ -11,4 +12,5 @@ podman run -d -it --privileged=true --rm -v /mnt/wslg/.X11-unix:/tmp/.X11-unix\
                -e WAYLAND_DISPLAY=wayland-0\
                -e XDG_RUNTIME_DIR=/mnt/wslg/runtime-dir\
                -e PULSE_SERVER=unix:/mnt/wslg/PulseServer\
+               --network gitkraken_internal\
                ghcr.io/whynotea/gitkraken:latest /usr/share/gitkraken/gitkraken --no-sandbox --disable-gpu
